d62dd74f05aa9c3999616fbe05af12392dddc552
Sam Acuna <samueleishion@gmail.com>
2013-06-07 17:32:23 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?
+?>
+<!DOCTYPE html>
+<html><head>
+ <title></title>
+ <link rel="stylesheet" type="text/css" href="_stys/global.css">
+ <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=visualization&sensor=false"></script>
+ <script src="_scrs/jquery.js"></script>
+ <script src="_scrs/main.js"></script>
+</head><body>
+
+<div id="map"> </div>
+
+</body></html>
