0d49d835302c8214e56a0062842b78253b58e6a9
Sam Acuna <samueleishion@gmail.com>
2013-06-10 12:20:11 -0500

    added some styles for general view, map, nav, search and buttons

--- _scrs/main.js
+++ _scrs/main.js
@@ -1,3 +1,105 @@
-$(document).ready(function() {
+var VIEWPOINTS = 0; 
+var VIEWHEATMAP = 1; 
+
+var map; 
+var view; 
+var curr; 
+
+function initializeMap() {
+	var center = new google.maps.LatLng(32.84,-22.67); 
+	var options = {
+		zoom: 3, 
+		center: center, 
+		mapTypeId: google.maps.MapTypeId.ROADMAP, 
+		streetViewControl:false, 
+		mapTypeControl:false 
+	}; 
 	
+	map = new google.maps.Map(document.getElementById('map'), options); 
+	map.set('styles', [
+		{
+			featureType:'all', 
+			elementType:'all', 
+			stylers: [
+				{ visibility: 'simplified' }, 
+				{ hue: '#d5bc96' }, 
+				{ gamma: 2 }, 
+				{ saturation: 32 }, 
+				{ lightness: 20 }
+			]
+		}, {
+			featureType:'water', 
+			elementType:'geometry', 
+			stylers: [
+				{ color: '#9ec2fd' }, 
+				{ weight: 1.6 }
+			]
+		}, {
+			featureType:'all', 
+			elementType:'labels', 
+			stylers: [
+				{ visibility: 'off' }
+			]
+		}, {
+			featureType:'landscape.natural', 
+			elementType:'all', 
+			stylers: [
+				{ color:'#e8e3da' }
+			] 
+		}
+	]);
+	
+	view =  VIEWPOINTS; 
+	$('.maptype #p').css('color','#000'); 
+	$('.maptype #h').css('color','#999');
+}
+
+function selectViewType() {
+	$('.maptype #p').click(function() {
+		view = VIEWPOINTS; 
+		$('.maptype #p').css('color','#000');
+		$('.maptype #h').css('color','#999');  
+	}); 
+	
+	$('.maptype #h').click(function() {
+		view = VIEWHEATMAP; 
+		$('.maptype #h').css('color','#000');
+		$('.maptype #p').css('color','#999');
+	}); 
+}
+
+function activateSearch() {
+	$('.options #s').click(function() {
+		$('.search').fadeIn();
+		$('.search #searchbox').focus();  
+	}); 
+	
+	$('.search #searchbox').keyup(function(e) {
+		if($('.search #searchbox').val().length>0) {
+			$('.search #searchbutton').val('Map it');
+		} else $('.search #searchbutton').val('Back');  
+		if(e.which==13) search($('.search #searchbox').val()); 
+	});
+	$('.search #searchbutton').click(function() {
+		search($('.search #searchbox').val()); 
+	}); 
+}
+
+function search(word) {
+	if(curr!='') {
+		curr = word;
+		showCurrent();
+	} 
+	$('.search').fadeOut();
+	$('.search #searchbox').delay(1000).val('');   
+}
+
+function showCurrent() {
+	$('.current #word').html('<b>'+curr+'</b>'); 
+}
+
+$(document).ready(function() {
+	initializeMap(); 
+	selectViewType(); 
+	activateSearch(); 
 }); 
--- index.php
+++ index.php
@@ -5,10 +5,37 @@
  <title></title>
  <link rel="stylesheet" type="text/css" href="_stys/global.css">
  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=visualization&sensor=false"></script>
- <script src="_scrs/jquery.js"></script>
+ <script src="_scrs/jquery.min.js"></script>
  <script src="_scrs/main.js"></script>
 </head><body>
 
+<!-- Navigation Bar -->
+<nav>
+ <div class="logo"></div>
+ <span id="title">WIKIWORLD</span> 
+ <div class="maptype">
+  <span id="p" alt="View data as points">o</span>
+  <span id="h" alt="View data as heatmap">f</span>
+ </div> 
+ <div class="options">
+  <span id="s" alt="Search for a word">s</span>
+  <span id="r" alt="Random word">/</span>
+  <span id="w" alt="Play a game">w</span>
+ </div>
+ <div class="more">
+  <span id="i" alt="Information">i</span>
+ </div>
+</nav>
+<!-- Current word display -->
+<div class="current">
+ Current word: <span id="word"> </span>
+</div>
+<!-- Word search -->
+<div class="search">
+ <input type="text" id="searchbox" placeholder="Type in a word from wikipedia!">
+ <input type="submit" id="searchbutton" value="Back">
+</div>
+<!-- GoogleMap -->
 <div id="map"> </div>
 
 </body></html>
